23c36f8d3785d5c49e60be520ffa82b38b194e91
Oleksandr Shamalo <shamalo1989@gmail.com>
2025-03-10 14:04:48 +0200

    created props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import Header from './components/Header';
 import Drawer from './components/Drawer';
 import './App.css';
 
+const arr = [
+  { title: 'Чоловічі кросівки Nike Blazer Mid Suede', price: 4999, imageUrl: '/img/goods/1.jpg' },
+  { title: 'Чоловічі кросівки Nike Air Max 270', price: 6499, imageUrl: '/img/goods/2.jpg' },
+  { title: 'Чоловічі кросівки Nike Blazer Classic', price: 4599, imageUrl: '/img/goods/3.jpg' },
+  { title: 'Чоловічі кросівки Puma X Aka Boku Future Rider', price: 7299, imageUrl: '/img/goods/4.jpg' },
+];
+
 function App() {
   return (
     // кошик з боку
@@ -23,10 +30,11 @@ function App() {
           </div>
 
           <div className='goods'>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+
+            {arr.map((obj) => (
+              <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
+            ))}
+
           </div>
 
         </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,25 @@
-function Card() {
+function Card(props) {
+
+    const onClickButton = () => {
+        alert("нажали на плюс");
+    }
+
     return (
         <div className="card">
             <div className="favorite">
                 <img src="/img/heart-unlike.svg" alt="unliked" />
             </div>
 
-            <img width={133} height={112} src="/img/goods/1.jpg" alt="sneakers" />
-            <h5>Чоловічі кросівки Nike Blazer Mid Suede</h5>
+            <img width={133} height={112} src={props.imageUrl} alt="sneakers" />
+            <h5>{props.title}</h5>
             <div className='d-flex justify-between align-center'>
                 <div className='d-flex flex-column'>
                     <span>Ціна: </span>
-                    <b>4999 грн.</b>
+                    <b>{props.price} грн.</b>
                 </div>
-                <button className='button'><img width={11} height={11} src="/img/add-to-cart.svg" alt='plus' /></button>
+                <button className='button' onClick={onClickButton}>
+                    <img width={11} height={11} src="/img/add-to-cart.svg" alt='plus' />
+                </button>
             </div>
         </div>
     );
